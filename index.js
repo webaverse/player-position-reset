4fa2702efbfaae7a448f52226838479747499086
aryasalehi <ariasalehidev@gmail.com>
2022-12-09 15:59:53 +0330

    fix div addition bug

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-import metaversefile from 'metaversefile';
-import * as THREE from 'three';
+import metaversefile from "metaversefile";
+import * as THREE from "three";
 
-const {
-  useApp,
-  useFrame,
-  useCleanup,
-  useLocalPlayer
-} = metaversefile;
+const {useApp, useFrame, useCleanup, useLocalPlayer} = metaversefile;
 
+const fadeDuration = 1000;
 const startVector = new THREE.Vector3(-Infinity, -Infinity, -Infinity);
 const endVector = new THREE.Vector3(Infinity, Infinity, Infinity);
 const resetVector = new THREE.Vector3(0, 0, 0);
 
 const _isInRange = (vector, start, end) => {
-  return vector.x >= start.x && vector.x <= end.x &&
-    vector.y >= start.y && vector.y <= end.y &&
-    vector.z >= start.z && vector.z <= end.z;
-}
+  return (
+    vector.x >= start.x &&
+    vector.x <= end.x &&
+    vector.y >= start.y &&
+    vector.y <= end.y &&
+    vector.z >= start.z &&
+    vector.z <= end.z
+  );
+};
 
-export default (e) => {
+let isResetting = false;
+
+export default e => {
   const app = useApp();
   const player = useLocalPlayer();
 
-  app.name = 'player-position-reset';
+  app.name = "player-position-reset";
 
   const _setVectorByComponent = (vector, name) => {
     const component = app.getComponent(name);
@@ -36,38 +39,60 @@ export default (e) => {
     }
   };
 
-  _setVectorByComponent(startVector, 'start');
-  _setVectorByComponent(endVector, 'end');
-  _setVectorByComponent(resetVector, 'reset');
+  _setVectorByComponent(startVector, "start");
+  _setVectorByComponent(endVector, "end");
+  _setVectorByComponent(resetVector, "reset");
 
-  const _resetPlayerPosition = () => {
+  const _addFadeoutDiv = () => {
     // Create a full screen white opaque overlay div over the entire screen, with opacity 0
-    const div = document.createElement('div');
-    div.style.position = 'absolute';
-    div.style.left = '0px';
-    div.style.top = '0px';
-    div.style.width = '100%';
-    div.style.height = '100%';
-    div.style.backgroundColor = 'white';
-    div.style.opacity = '0';
+    const div = document.createElement("div");
+    div.style.position = "absolute";
+    div.style.left = "0px";
+    div.style.top = "0px";
+    div.style.width = "100%";
+    div.style.height = "100%";
+    div.style.backgroundColor = "white";
+    div.style.opacity = "0";
     document.body.appendChild(div);
 
-    // then fade up to 1 over 1 second
-    div.style.transition = 'opacity 1s';
-    div.style.opacity = '1';
-
-    // on transition end, call setPosition on the player with player.characterPhysics.setPosition(resetVector);
-
-    setTimeout(() => {
-      player.characterPhysics.setPosition(resetVector);
-        // then fade back down to 0 over 1 second, then remove the div
-        div.style.transition = 'opacity 1s';
-        div.style.opacity = '0';
-        setTimeout(() => {
-          document.body.removeChild(div);
-        }, 1000);
-    }, 1000);
+    return div;
   };
+
+  const _setFadeoutAnimationStyle = div => {
+    div.style.transition = `opacity ${fadeDuration}ms ease-out`;
+  };
+  const _fadeOut = div => {
+    div.style.opacity = "0";
+  };
+  const _fadeIn = div => {
+    div.style.opacity = "1";
+  };
+
+  const _resetPlayerPosition = () => {
+    // only run once when this is triggered
+    if (!isResetting) {
+      isResetting = true;
+
+      const div = _addFadeoutDiv();
+      _setFadeoutAnimationStyle(div);
+
+      _fadeIn(div);
+
+      setTimeout(() => {
+        // console.log("resetting player position");
+        player.characterPhysics.setPosition(resetVector);
+
+        _fadeOut(div);
+
+        // after the animation remove the element
+        setTimeout(() => {
+          isResetting = false;
+          document.body.removeChild(div);
+        }, fadeDuration);
+      }, fadeDuration);
+    }
+  };
+
   useFrame(() => {
     const playerPosition = player.position;
     const inRange = _isInRange(playerPosition, startVector, endVector);
@@ -76,8 +101,7 @@ export default (e) => {
     }
   });
 
-  useCleanup(() => {
-  });
+  useCleanup(() => {});
 
   return app;
 };
